adb3586d4b90cee02a748712eaeb0a01f3298df3
Carlos Jobe <carlos.jobe@g.austincc.edu>
2019-11-07 11:58:54 -0600

    Update LED_Button_Test.py

--- LED_Button_Test.py
+++ LED_Button_Test.py
@@ -2,20 +2,23 @@ from gpiozero import LED, Button, TonalBuzzer
 from gpiozero.tones import Tone
 from time import sleep
 
+
+
 ledG = LED(26)
 ledR = LED(21)
 ledB = LED(20)
-ledY = LED(6)
+ledY = LED(19)
 
-butG = Button(22)
+butG = Button(6)
 butR = Button(16)
 butB = Button(12)
-butY = Button(5)
+butY = Button(13)
 
 bz = TonalBuzzer(25)
 
-time1 = 0.25
-time2 = 0.125
+time1 = 0.1
+time2 = 0.05
+time3 = 0.025
 
 # define output functions
 def green(time):
@@ -54,6 +57,15 @@ def yellow(time):
     bz.stop()
     return;
 
+def fail():
+    i=0
+    while (i<=15):
+        blue(time3)
+        green(time3)
+        yellow(time3)
+        red(time3)
+        i=i+1    
+
 # Startup sequence
 green(time1)
 red(time1)
@@ -63,6 +75,9 @@ green(time2)
 red(time2)
 blue(time2)
 yellow(time2)
+
+fail()
+    
 
 while True:
     if butG.is_pressed:
