501504b85f92a9baf439fbbd843a1061eb76fa40
DiegoE1 <fred.espinosa@g.austincc.edu>
2019-11-14 11:23:56 -0600

    Latest changes to python code

--- simon.py
+++ simon.py
@@ -1,16 +1,19 @@
-#import necessary libaries
+#import necessary libraries
+import random
+import time
 
 #global variables
-int difficulty = 0
+difficulty = 0
+timer = 0.0
 expectedSequence = []
 userSequence = []
 
 #global tracking variables
-int score
+score = 0
 
-int easyHighScore
-int mediumHighScore
-int hardHighScore
+easyHighScore = 0
+mediumHighScore = 0
+hardHighScore = 0
 
 #main program
 while(power is on):
@@ -18,28 +21,34 @@ while(power is on):
     wait_for_response() #from imported library, wait for start button to be pressed
     difficulty = #value from game board, from difficulty slider/button
     if(difficulty == 0): #easy difficulty
-        int timeAllowed = determineTimeAllowed(difficulty)
-        #to be used to compare if user took longer or shorter than the allowed time
-        #(or if imported functions have the ability to set the time allowed for a response)
+        determineTimerLength()
+        count = 0
+        
         bool continueGame = true #condition that is changed within the while if the sequences are not the same
         while(continueGame):
+            #random num
             int num = generateRandNum()
             expectedSequence.append(num)
+
+            #output expected sequence
             outputSequenceTones()
-            #start timer
-            wait_for_response()
-            #stop timer
-            #compare time length with timeAllowed #if less than timeAllowed; check sequence #else greater than timeAllowed; gameOver
-            #append userSequence with what button was pressed; based on input
-            if(expectedSequence != userSequence):
-                continueGame = false
-        outputGameOver()
+            secondCondition = True
+            while count < len(expectedSequence) and secondCondition
+                wait_for_response() #get user input
+
+                if(userInput != expectedSequence[count]):
+                    secondCondition = False
+                else:
+                    userSequence.append(userInput)
+                    count+=1
+
+            #checking if user is correct
+            checkIfUserCorrect()
     elif(difficulty == 1):
         #copy paste code once finished implementing easy difficulty
-        #not necssary, difficulty will just alter a global timer variable
     elif(difficulty == 2):
         #copy paste code once finished implementing other difficulties
-    #if we want more difficulties we can add more; just adjust the times within determineTimeAllowed()
+    #if we want more difficulties we can add more; just adjust the timer within determineTimeAllowed()
 
 #functions
 def startUpSequence():
@@ -49,34 +58,66 @@ def startUpSequence():
 
 def generateRandNum():
     #generate a random num; will need rand library
-    #returns an int 0-3
+    return random.randint(0,3)
+    #returns an int (0-3)
 
 def outputSequenceTones():
     for i in expectedSequence:
         #play sound for each signal
-        #likely need to set 0-3 to play a certain signal
+        if(i == 0):
+            #play sound 1
+        elif(i == 1):
+            #play sound 2
+        elif(i == 2):
+            #play sound 3
+        elif(i == 3):
+            #play sound 4
+        time.sleep(timer) #delay in between output of tones
     #returns nothing
+
+def checkIfUserCorrect():
+    #checking if user is correct
+    if userSequence != expectedSequence:
+        #user sequence does not match
+        secondCondition = False
+        outputGameOver(difficulty)
+    else:
+        #user sequence matches, increment score and clear user sequence
+        print("Correct\n")
+        score+=1
+        userSequence.clear()
 
 def outputGameOver():
     #will play harsh tones
     #handle any global variables to be reset
     #saving data into high score if score higher
     #maybe display their score and their high score for that difficulty
+
+    #print high score
+        if(difficulty == 0 and score > easyHighScore):            
+            easyHighScore = score
+        elif(difficulty == 1 and score > mediumHighScore):
+            mediumHighScore = score
+        elif(difficulty == 2 and score > hardHighScore):
+            hardHighScore = score
+        #print("\nHigh Score: " + str(highScore) + ", " + "Score: " + str(score))
+        
+        #determine if they want to continue or quit
+        endInput = input("Try Again?(Y to continue, N to quit): ")
+        if(endInput == "Y" or endInput == "y"):
+            expectedSequence.clear()
+            userSequence.clear()
+            score = 0
+        elif(endInput == "N" or endInput == "n"):
+            condition = False
+            print("\n\nEnd Program")
     #returns nothing
 
-def determineTimeAllowed(int difficulty):
-    # difficulty will change delay between output instances making the 
-    # output sequece faster for higher difficulty
+def determineTimerLength():
     if(difficulty == 0):
-        #time allowed should be for example 5 seconds
+        timer = 3.0
     elif (difficulty == 1):
-        #time allowed should be for example 3 seconds
+        timer = 1.5
     elif (difficulty == 2):
-        #time allowed should be for example 1.5 seconds
-
-    #returns the time that should be allowed
-
-
-
-
-        
+        timer = 0.5
+    #returns nothing
