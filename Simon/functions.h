9fec6166788174a0c9b0d1c88e9c3c701ba857f0
DiegoE1 <fred.espinosa@g.austincc.edu>
2019-11-16 12:20:21 -0600

    Added function prototypes and directives

--- Simon/functions.h
+++ Simon/functions.h
@@ -1,4 +1,17 @@
 #ifndef SIMON_FUNCTIONS_H
 #define SIMON_FUNCTIONS_H
 
+#include <stdlib.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void startUpSequence();
+int generateRandomNum();
+void outputSequenceTones();
+bool checkIfUserCorrect();
+void outputGameOver();
+void determineTimerLength();
+
 #endif //SIMON_FUNCTIONS_H
