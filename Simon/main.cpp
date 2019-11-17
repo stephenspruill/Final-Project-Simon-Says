43008c5e5b97e6358d9265c43450728eefb6de31
jaguilar81496 <57151142+jaguilar81496@users.noreply.github.com>
2019-11-17 10:29:51 -0600

    Update main.cpp

--- Simon/main.cpp
+++ Simon/main.cpp
@@ -1,7 +1,76 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime>
 #include "main.h"
+using namespace std;
 
-//Main game loop will exist here
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+int difficulty = 0;
+int timer = 0.0;
+vector<int> expectedSequence;
+vector<int> userSequence;
+
+
+//global tracking variables
+int score = 0;
+
+int easyHighScore = 0;
+int mediumHighScore = 0;
+int hardHighScore = 0;
+
+
+
+int main()
+{
+	bool power = true;
+	while (power == true)
+	{
+		startUpSequence();
+		waitForResponse();
+		difficulty = // value from game board, from difficulty slider/button
+		if (difficulty == 0) // easy difficulty
+		{
+			determineTimerLength();
+			int count = 0;
+		
+		
+			bool continueGame = true;
+			while (continueGame == true)
+			{
+				int num = generateRandomNum();
+				expectedSequence.push_back(num);
+			
+				outputSequenceTones();
+				result = true;
+				while (count < expectedSequence.size() && result)
+				{
+					waitForResponse();
+				
+					if(userInput != expectedSequence[count])
+					{
+						result = false;
+					}
+					else
+					{
+						userSequence.push_back(userInput);
+						count++;
+					}
+				checkIfUserCorrect();
+		
+				
+				}
+			}
+		else if( difficulty == 1)
+		{
+			//copy paste code once finished implementing easy difficulty
+		}
+		else if(difficulty == 2)
+		{
+			//copy paste code once finished implementing easy difficulty
+		}
+		}
+	}
+	
+	
+	
+	
 }
