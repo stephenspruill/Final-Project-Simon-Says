b2545788755be0dfa2b95f52129f7d8273b31a0b
Carlos Jobe <carlos.jobe@g.austincc.edu>
2019-11-19 15:14:43 -0600

    Update LED_Button_Test.cpp

--- LED_Button_Test.cpp
+++ LED_Button_Test.cpp
@@ -1,51 +1,61 @@
 // to compile, run the following in terminal
 //g++ -std=c++0x btest.cpp -obtest -lwiringPi
 #include <iostream>
+#include <stdio.h>
 #include <wiringPi.h>
-#include <time.h>
 
 using namespace std;
 
 int main()
 {
 	wiringPiSetupGpio ();
-	pinMode (26, OUTPUT);
-	pinMode (21, OUTPUT);
-	pinMode (20, OUTPUT);
-	pinMode (19, OUTPUT);
 	
-	pinMode (6, INPUT);
-	pinMode (16, INPUT);
-	pinMode (12, INPUT);
-	pinMode (13, INPUT);
+	pinMode (26, OUTPUT);	//Green LED
+	pinMode (21, OUTPUT);	//Red LED
+	pinMode (20, OUTPUT);	//Blue LED
+	pinMode (19, OUTPUT);	//Yellow LED
+	
+	pinMode (6, INPUT);		//Green Button
+	pinMode (4, INPUT);		//Red Button
+	pinMode (12, INPUT);	//Blue Button
+	pinMode (13, INPUT);	//Yellow Button
 
+	digitalWrite (26, LOW);
+	digitalWrite (21, LOW);
+	digitalWrite (20, LOW);
+	digitalWrite (19, LOW);
+	
 	while(1)
 	{
-
-		if(digitalRead(6) == 1)
+		if(digitalRead(6) == 0)	//green
 		{
-		digitalWrite (26, HIGH);
-		delay (500);
-		digitalWrite (26, LOW);
+			cout << "Green on" << endl;
+			digitalWrite(26, HIGH);
+			delay (500);
+			digitalWrite (26, LOW);
 		}
-		else if(digitalRead(16) == 1)
+		if(digitalRead(4) == 0)	//red
 		{
-		digitalWrite (21, HIGH);
-		delay (500);
-		digitalWrite (21, LOW);
+			cout << "Red on" << endl;
+			digitalWrite(21, HIGH);
+			delay (500);
+			digitalWrite (21, LOW);
 		}
-		else if(digitalRead(12) == 1)
+		if(digitalRead(13) == 0)	//yellow
 		{
-		digitalWrite (20, HIGH);
-		delay (500);
-		digitalWrite (20, LOW);
-		}		
-		else if(digitalRead(13) == 1)
+			cout << "Yellow on" << endl;
+			digitalWrite(19, HIGH);
+			delay (500);
+			digitalWrite (19, LOW);
+		}
+		if(digitalRead(12) == 0)	//blue
 		{
-		digitalWrite (19, HIGH);
-		delay (500);
-		digitalWrite (19, LOW);
+			cout << "Blue on" << endl;
+			digitalWrite(20, HIGH);
+			delay (500);
+			digitalWrite (20, LOW);
 		}
 	}
+
 	return 0;
 }
