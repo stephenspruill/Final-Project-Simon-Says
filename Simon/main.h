915481c52dab475482a381f9f8dd6d713d672188
DiegoE1 <fred.espinosa@g.austincc.edu>
2019-11-19 13:21:52 -0600

    Moved function prototypes into main.h

--- Simon/main.h
+++ Simon/main.h
@@ -1,9 +1,15 @@
 #ifndef SIMON_MAIN_H
 #define SIMON_MAIN_H
 
-#include "functions.h"
 #include <iostream>
-using std::cout;
-using std::endl;
+using namespace std;
+
+//Main Functions
+void startUpSequence();
+int generateRandomNum();
+void outputSequenceTones();
+void checkIfUserCorrect();
+void outputGameOver();
+void determineTimerLength();
 
 #endif //SIMON_MAIN_H
