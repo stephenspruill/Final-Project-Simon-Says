2a1f4d9253b1f49cdc95a5ead1cc79286921841e
DiegoE1 <fred.espinosa@g.austincc.edu>
2019-11-19 13:21:01 -0600

    Moved functions into main file

--- Simon/main.cpp
+++ Simon/main.cpp
@@ -2,25 +2,19 @@
 #include <iomanip>
 #include <ctime>
 #include "main.h"
-using namespace std;
 
+//Global Variables
 int difficulty = 0;
 int timer = 0.0;
+
+//Main Vectors to Be Compared
 vector<int> expectedSequence;
 vector<int> userSequence;
+
+//Scoring Vectors
 vector<int> easyHighSequence;
 vector<int> mediumHighSequence;
 vector<int> hardHighSequence;
-
-
-//global tracking variables
-int score = 0;
-
-int easyHighScore = 0;
-int mediumHighScore = 0;
-int hardHighScore = 0;
-
-
 
 int main()
 {
@@ -71,9 +65,68 @@ int main()
 			//copy paste code once finished implementing easy difficulty
 		}
 		}
-	}
-	
-	
-	
-	
+	}	
 }
+
+void startUpSequence(){
+    //output lights to flash
+}
+
+int generateRandomNum(){
+    int randNum = 0;
+    randNum = rand() % 3;
+    return randNum
+}
+
+void outputSequenceTones(){
+    //will need access to expected sequence
+    for(int i = 0; i < /*size*/; i++){
+        if(/*expectedSequence[i] == 0*/){
+            //play sound for that certain button
+        }else if (/*expectedSequence[i] == 1*/){
+            //play sound for that certain button
+        }else if (/*expectedSequence[i] == 2*/){
+            //play sound for that certain button
+        }else if (/*expectedSequence[i] == 3*/){
+            //play sound for that certain button
+        }
+    }
+}
+
+void checkIfUserCorrect(){
+    bool wasUserCorrect = true;
+    for(int i = 0; i < /*size*/ && wasUserCorrect; i++){
+        if(/*expectedSequence[i] != userSequence[i]*/){
+            //user incorrect
+            wasUserCorrect = false;
+            outputGameOver();
+        } else {
+            //user was correct
+            /*score++*/
+            /*userSequence.clear()*/
+        }
+    }
+}
+
+void outputGameOver(){
+    //play harsh tones
+
+    if(/*difficulty == 0 && score > easyHighScore*/){
+        /*easyHighScore = score*/
+    }else if(/*difficulty == 1 && score > mediumHighScore*/){
+        /*mediumHighScore = score*/
+    }else if(/*difficulty == 2 && score > hardHighScore*/){
+        /*hardHighScore = score*/
+    }
+}
+
+void determineTimerLength(){
+    if(/*difficulty == 0*/){
+        /*timer = 3.0*/
+    }else if(/*difficulty == 1*/){
+        /*timer = 1.5*/
+    }else if(/*difficulty == 2*/){
+        /*timer = 0.5*/
+    }
+}
+
