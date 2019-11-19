7285a805674530ef5bb3c5673ff52046f58629df
Carlos Jobe <carlos.jobe@g.austincc.edu>
2019-11-19 11:49:35 -0600

    Update LED_Button_Test.cpp

--- LED_Button_Test.cpp
+++ LED_Button_Test.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main()
 {
-	wiringPiSetup ();
+	wiringPiSetupGpio ();
 	pinMode (26, OUTPUT);
 	pinMode (21, OUTPUT);
 	pinMode (20, OUTPUT);
@@ -19,37 +19,33 @@ int main()
 	pinMode (12, INPUT);
 	pinMode (13, INPUT);
 
-
-
-
-
 	while(1)
 	{
 
-	digitalWrite(21, HIGH); delay (500);
-	digitalWrite(21, LOW); delay (500);
-
-
-		/*if(digitalRead(6) == 1)
+		if(digitalRead(6) == 1)
 		{
-		digitalWrite (26, !digitalRead(26));
+		digitalWrite (26, HIGH);
 		delay (500);
+		digitalWrite (26, LOW);
 		}
 		else if(digitalRead(16) == 1)
 		{
-		digitalWrite (21, !digitalRead(21));
+		digitalWrite (21, HIGH);
 		delay (500);
+		digitalWrite (21, LOW);
 		}
 		else if(digitalRead(12) == 1)
 		{
-		digitalWrite (20, !digitalRead(20));
+		digitalWrite (20, HIGH);
 		delay (500);
+		digitalWrite (20, LOW);
 		}		
 		else if(digitalRead(13) == 1)
 		{
-		digitalWrite (19, !digitalRead(19));
+		digitalWrite (19, HIGH);
 		delay (500);
-		}*/
+		digitalWrite (19, LOW);
+		}
 	}
 	return 0;
 }
